5693d9a861b55ddf8121dcc4f409b5b0d379327f
MilenaHristova <milena.hristova12@gmail.com>
2017-11-10 13:57:34 +0200

    inform

--- js/calculate.js
+++ js/calculate.js
@@ -1,21 +1,17 @@
 function Calculate() {
     let gender = document.getElementById('gender').value;
     let weight = document.getElementById('weight').value;
-    let beverageType = document.getElementById('beverage').value;
-    let beveragesNumber = document.getElementById('numberOfBeverages').value;
+    let unitsNumber = document.getElementById('numberOfBeverages').value;
     let time = document.getElementById('time').value;
 
     let genderConst = 0;
     if(gender == 'Male')genderConst = 0.68;
     else genderConst = 0.55;
 
-    let servingInGramms = 500;
-    let alcoholPercentage = 10;
-
-    let alcoholContent = servingInGramms * alcoholPercentage / 100;
+    let alcoholContent = 20*unitsNumber;
 
     let bodyWeight = weight * 1000;
-    let bac = alcoholContent/(bodyWeight * genderConst) * 100;
+    let bac = alcoholContent/(bodyWeight * genderConst) * 100 - time*0.015;
 
 
     document.getElementById('result').value = bac.toString();
